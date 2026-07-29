b2b2d58ec7ababaf4c5891a1e6e861c7c9801856
Heitor Tetzner Pereira <heitortetzner2015@gmail.com>
2026-07-28 21:01:36 -0300

    feat: adiciona novas tabelas na database

---
+++ database/migrations/2026_07_28_125005_create_address_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_usuario');
+            $table->string('cep');
+            $table->string('logradouro');
+            $table->string('complemento')->nullable();
+            $table->string('numero');
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->timestamps();
+
+            $table->foreign('id_usuario')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enderecos');
+    }
+};
