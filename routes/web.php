274b6ae6685a38c670efa869b149e2940ae03b10
Heitor Tetzner Pereira <heitortetzner2015@gmail.com>
2026-08-01 11:58:58 -0300

    feat : implementa login e registro

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", [HomeController::class, "index"])->name("home");
-
-Route::get("/search", [HomeController::class, "search"])->name("products.search");
 
 Route::get("/welcome", function () {
     return view("welcome");
@@ -28,6 +25,8 @@ Route::middleware("auth")->group(function () {
     Route::delete("/profile", [ProfileController::class, "destroy"])->name(
         "profile.destroy",
     );
+
+    Route::get("/", [HomeController::class, "index"])->name("home");
 });
 
 require __DIR__ . "/auth.php";
