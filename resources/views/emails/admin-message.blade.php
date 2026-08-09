fbb75b5b2eeda59930e9b284f782384a9206d200
Heitor Tetzner Pereira <heitortetzner2015@gmail.com>
2026-08-08 22:36:42 -0300

    feat: cria página de email

---
+++ resources/views/emails/admin-message.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>{{ $subject }}</title>
+</head>
+
+<body>
+
+    <h1>{{ $subject }}</h1>
+
+    <p>
+        {{ $message }}
+    </p>
+
+</body>
+
+</html>
