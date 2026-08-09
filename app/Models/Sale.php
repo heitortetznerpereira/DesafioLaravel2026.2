46a14cdb00424834b5664ed59b6f485e6e815a52
Heitor Tetzner Pereira <heitortetzner2015@gmail.com>
2026-08-09 16:17:04 -0300

    feat: cria factory de sales

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Sale extends Model
 {
     //
+    use HasFactory;
     protected $fillable = ["product_id", "buyer_id", "seller_id", "price"];
 
     public function product()
---
+++ database/factories/SaleFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sale>
+ */
+class SaleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "product_id" => Product::inRandomOrder()->first()->id,
+            "buyer_id" => User::inRandomOrder()->first()->id,
+            "seller_id" => User::inRandomOrder()->first()->id,
+            "price" => fake()->randomFloat(2, 10, 1000),
+        ];
+    }
+}
